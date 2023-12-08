6c9c93efc204c19135ceee1d5e96440bc11d57b7
staticclown <george38691@gmail.com>
2023-12-08 22:59:31 +0530

    migrations done

--- Tutor/TutorApp/views.py
+++ Tutor/TutorApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('hi')
 
 # Create your views here.
