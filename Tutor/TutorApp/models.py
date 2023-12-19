4a8389fc5ae2472eb219baed9e589628322c565d
staticclown <george38691@gmail.com>
2023-12-19 21:16:35 +0530

    added primary key

--- Tutor/TutorApp/models.py
+++ Tutor/TutorApp/models.py
@@ -10,4 +10,4 @@ class UserSignup(models.Model):
     signupname = models.CharField(max_length=30)
     signuppassword = models.CharField(max_length=30)
     signupdob = models.CharField(max_length=30)
-    signupemail = models.CharField(max_length=30)
+    signupemail = models.CharField(max_length=30, primary_key=True)
