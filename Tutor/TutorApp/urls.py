29b8470345c0037f49346f9a4116e8b0f9c10d48
staticclown <george38691@gmail.com>
2024-02-13 20:01:54 +0530

    added path

--- Tutor/TutorApp/urls.py
+++ Tutor/TutorApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("login/", views.UserLoginView.as_view()),
     path("signup/",views.UserSignupView.as_view()),
+    path("image/",views.ImageView.as_view()),
     path("video", views.home),
 ]
