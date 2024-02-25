2eae1620c2045ec0a783d31d215c2a38d2684896
staticclown <george38691@gmail.com>
2024-02-25 18:14:07 +0530

    update

--- Tutor/TutorApp/urls.py
+++ Tutor/TutorApp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("image/", views.ImageEntry.as_view()),
     path("imgdelete/", views.ImageDelete.as_view()),
     path("userupdate/", views.UserUpdate.as_view()),
-    path("imageview/", views.Imageview.as_view()),
+    path("imageview/", views.ImageView.as_view()),
 ]
